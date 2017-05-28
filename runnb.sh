fa68567b5ed69657ba29a657045c3ba1f46cb3ec
GeorgiMateev <gmmateev@gmail.com>
2017-05-28 22:32:50 +0300

    Redirect output

--- runnb.sh
+++ runnb.sh
@@ -5,6 +5,6 @@ scriptname="$(basename $notebook .ipynb)".py
 
 # Give process a 4 hours to complete
 shutdown +240
-jupyter nbconvert --to script --execute ${notebook} && python ${scriptname}
+jupyter nbconvert --to script --execute ${notebook} && python ${scriptname} > stdout.txt 2> stderr.txt
 rm ${scriptname}
 shutdown now

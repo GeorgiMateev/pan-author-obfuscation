9c6d5dee920275b0df5d4e87bfd2c45da0aac406
Ubuntu <ubuntu@ip-172-31-21-19.eu-west-1.compute.internal>
2017-05-28 20:10:53 +0000

    Fixes on the run script

--- runnb.sh
+++ runnb.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+]#!/bin/sh
 # First argument is the notebook you would like to run
 notebook=$1
 scriptname="$(basename $notebook .ipynb)".py
 
 # Give process a 4 hours to complete
-shutdown +240
-jupyter nbconvert --to script --execute ${notebook} && python ${scriptname} > stdout.txt 2> stderr.txt
-rm ${scriptname}
-shutdown now
+sudo shutdown +240
+sudo -E env "PATH=$PATH" jupyter nbconvert --to script --execute ${notebook} && ipython ${scriptname} > stdout.txt 2> stderr.txt
+sudo rm ${scriptname}
+sudo shutdown now

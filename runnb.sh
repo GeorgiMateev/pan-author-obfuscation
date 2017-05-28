9d92a45ca7310fe871309f3860c2efdafc1a7ab9
GeorgiMateev <gmmateev@gmail.com>
2017-05-28 22:27:53 +0300

    Added run script

---
+++ runnb.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# First argument is the notebook you would like to run
+notebook=$1
+scriptname="$(basename $notebook .ipynb)".py
+
+# Give process a 4 hours to complete
+shutdown +240
+jupyter nbconvert --to script --execute ${notebook} && python ${scriptname}
+rm ${scriptname}
+shutdown now

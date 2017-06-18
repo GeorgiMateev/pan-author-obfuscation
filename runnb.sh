f612129c56e55831126d6364e32bfd640ee8205e
Ubuntu <ubuntu@ip-172-31-21-19.eu-west-1.compute.internal>
2017-06-18 19:56:14 +0000

    Fix hang up whn using magic function

--- runnb.sh
+++ runnb.sh
@@ -5,6 +5,6 @@ scriptname="$(basename $notebook .ipynb)".py
 
 # Give the process some minutes complete
 # sudo shutdown +120
-sudo -E env "PATH=$PATH" jupyter nbconvert --to script --execute ${notebook} && ipython ${scriptname} > stdout.txt 2> stderr.txt
-#sudo rm ${scriptname}
-#sudo shutdown now
+sudo -E env "PATH=$PATH" jupyter nbconvert --to script ${notebook} && ipython ${scriptname} > stdout.txt 2> stderr.txt
+sudo rm ${scriptname}
+sudo shutdown now
